898fb7717014f43a8d73fa98be4715abffed12b7
amothm80 <amo.1980@gmail.com>
2024-11-20 21:47:15 +0300

    fibo merge

--- fibo.js
+++ fibo.js
@@ -1,26 +1,73 @@
-function fibo(num){
-    for (let i = 1; i <= num; i++){
-        let fibm2 = 0;
-        let fib = 1;
-        if ( i == 1){
-            console.log(0)
-            console.log(1)
-
-        }else{
-
-        }
+function fibo(num) {
+  let fib = 0;
+  let fibp = 0;
+  const fibArr = [];
+  for (let i = 1; i < num; i++) {
+    if (i == 1) {
+      fibArr.push(0);
+      fibArr.push(1);
+      fibp = 0;
+      fib = 1;
+    } else {
+      fib = fib + fibp;
+      fibp = fib - fibp;
+      fibArr.push(fib);
     }
+  }
+  return fibArr;
 }
 
-function fiboRec(num){
-    if (num == 0){
-        console.log(num)
-        return num;
-    }else{
-        let fibs = num + fibsRec(num-1);
-        console.log(fibs)
-        return fibs
-    }
+function fiboRec(num) {
+  if (num == 2) {
+    console.log(0);
+    console.log(1);
+    return { fib: 1, fibp: 0 };
+  } else {
+    let { fib, fibp } = fiboRec(num - 1);
+    fib = fib + fibp;
+    fibp = fib - fibp;
+    console.log(fib);
+    return { fib, fibp };
+  }
 }
 
-fiboRec(8)
+function mergeSort(arr) {
+  let arrLeft = [];
+  let arrRight = [];
+  let arrSorted = [];
+  if (arr.length > 1) {
+    arrLeft = mergeSort(arr.slice(0, arr.length / 2));
+    arrRight = mergeSort(arr.slice(arr.length / 2, arr.length));
+  } else {
+    return arr;
+  }
+
+  for (let i = 0; i < arrLeft.length; i++) {
+    let indexRight = 0;
+    let pushed = false;
+    for (let j = 0; j < arrRight.length; j++) {
+      if (arrLeft[i] < arrRight[j]) {
+        arrSorted.push(arrLeft[i]);
+        pushed = true
+        break;
+      } else {
+        arrSorted.push(arrRight[j]);
+        indexRight++;
+      }
+    }
+    arrRight = arrRight.splice(indexRight);
+    if (i + 1 >= arrLeft.length && arrRight.length > 0) {
+      for (let x = 0; x < arrRight.length; x++) {
+        arrSorted.push(arrRight[x]);
+      }
+    }
+    if (!pushed && arrRight.length < 1){
+        arrSorted.push(arrLeft[i])
+    }
+  }
+  return arrSorted;
+}
+
+// console.log(fibo(8))
+// fibonacci(8);
+console.log(mergeSort([105, 79, 100, 110]));
